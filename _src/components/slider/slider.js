4fbe37a45454b1ad01e1ad21d8217d00223d9a2b
Mads Thines <MadsThines@2830363.no-reply.drupal.org>
2016-09-14 10:39:24 +0200

    Added link support

--- _src/components/slider/slider.js
+++ _src/components/slider/slider.js
@@ -17,5 +17,38 @@ module.exports = function slider() {
       });
     });
 
+    // Jumps to next anchor point
+    (function() {
+      var delay = false;
+
+      $(document).on('mousewheel DOMMouseScroll', function(event) {
+        event.preventDefault();
+        if(delay) return;
+
+        delay = true;
+        setTimeout(function(){delay = false},200);
+
+        var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+        var a = $('.inner > *');
+
+        if(wd < 0) {
+          for(var i = 0 ; i < a.length ; i++) {
+            var t = a[i].getClientRects()[0].top;
+            if(t >= 40) break;
+          }
+        }
+        else {
+          for(var i = a.length-1 ; i >= 0 ; i--) {
+            var t = a[i].getClientRects()[0].top;
+            if(t < -20) break;
+          }
+        }
+        $('html,body').animate({
+          scrollTop: a[i].offsetTop
+        });
+      });
+    })();
+
   })(jQuery);
 };
