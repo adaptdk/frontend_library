1f9b9c0f850960115b492708e63b7866f4eee379
Mads Thines <MadsThines@2830363.no-reply.drupal.org>
2016-09-19 13:46:07 +0200

    Drupal Con slider, typo and items

--- _src/components/slider/slider.js
+++ _src/components/slider/slider.js
@@ -1,12 +1,23 @@
 module.exports = function slider() {
   (function ($) {
 
+    $(function () {
+      var $title = $('.title');
+      $title.click(function () {
+        if ($(this).hasClass('collapsed')) {
+          $(this).removeClass('collapsed');
+        } else {
+          $(this).addClass('collapsed');
+        }
+      });
+    });
+
     // Adds smooth scroll between anchor points
-    $(function() {
-      $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    $(function () {
+      $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
           var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
           if (target.length) {
             $('html, body').animate({
               scrollTop: target.offset().top
@@ -16,39 +27,6 @@ module.exports = function slider() {
         }
       });
     });
-
-    // Jumps to next anchor point
-    (function() {
-      var delay = false;
-
-      $(document).on('mousewheel DOMMouseScroll', function(event) {
-        event.preventDefault();
-        if(delay) return;
-
-        delay = true;
-        setTimeout(function(){delay = false},200);
-
-        var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-
-        var a = $('.inner > *');
-
-        if(wd < 0) {
-          for(var i = 0 ; i < a.length ; i++) {
-            var t = a[i].getClientRects()[0].top;
-            if(t >= 40) break;
-          }
-        }
-        else {
-          for(var i = a.length-1 ; i >= 0 ; i--) {
-            var t = a[i].getClientRects()[0].top;
-            if(t < -20) break;
-          }
-        }
-        $('html,body').animate({
-          scrollTop: a[i].offsetTop
-        });
-      });
-    })();
 
   })(jQuery);
 };
